ebd00e456a9fb82b4b56d9d110bb0834b2911d4b
jacknguyen <nguyenphucbao098@gmail.com>
2023-05-10 11:23:55 +0700

    fix test

--- backend/src/controllers/ticket.controller.js
+++ backend/src/controllers/ticket.controller.js
@@ -19,7 +19,7 @@ const createTicket = catchAsync(async (req, res) => {
 
 const payTicket = catchAsync(async (req, res) => {
   const result = await ticketService.payTicket(req.body.ticket_ids);
-  res.send(resul);
+  res.send(result);
 });
 
 const printTicket = catchAsync(async (req, res) => {
