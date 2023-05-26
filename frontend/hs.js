8115841253f64b8eb0266f17d3842efbc5d26fe5
Tien <73010955+tientran505@users.noreply.github.com>
2023-05-26 09:37:44 +0700

    refactor: clean code

---
+++ frontend/hs.js
@@ -0,0 +1,132 @@
+               <td class='text-primary'>100.000 vnđ</td>
+                </tr>
+              </table>
+              <hr />
+              <div>
+       
+    
+            </div>
+          </div> --}}
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+                  <td class='text-primary'>100.000 vnđ</td>
+                </tr>
+              </table>
+              <hr />
+              <div>
+                <img class='img-fluid' src='https://cdn1.nhatrangtoday.vn/images/photos/xe-phuong-trang-top.jpg'
+                  alt='Xe' />
+              </div>
+            </div>
+          </div> --}}
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+                  <td class='text-primary'>100.000 vnđ</td>
+                </tr>
+              </table>
+              <hr />
+              <div>
+                <img class='img-fluid' src='https://cdn1.nhatrangtoday.vn/images/photos/xe-phuong-trang-top.jpg'
+                  alt='Xe' />
+              </div>
+            </div>
+          </div> --}}
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  .container {
+    width: 80%;
+  }
+</style>
+
+<script src='/js/list.js'></script>{{> header}}
+
+<main class='container'>
+  {{> breadcrumb}}
+  <div class='card p-2 mt-4'>
+    {{> search}}
+  </div>
+  <div class='mt-5 row'>
+    <div class='col-md-3'>
+      <div class='card'>
+        <div class='card-header'>Filter</div>
+        <div class='card-body'>
+          <div class='mb-3'>
+            <label for='exampleFormControlInput1' class='form-label fw-bold'>Bus
+              Operator</label>
+            <select class='form-select' aria-label='Default select example' id="filter-bus-operator">
+              <option value="" selected>All</option>
+            </select>
+          </div>
+          <div class='mb-3'>
+            <label for='exampleFormControlInput1' class='form-label fw-bold'>Types of seat</label>
+            <div class='form-check'>
+              <input class='form-check-input' type='radio' value='0' name="typeOfSeat" />
+              <label class='form-check-label' for='flexCheckDefault'>
+                Limousine
+              </label>
+            </div>
+            <div class='form-check'>
+              <input class='form-check-input' type='radio' value='1' checked name="typeOfSeat" />
+              <label class='form-check-label' for='flexCheckChecked'>
+                Normal Seat
+              </label>
+            </div>
+
+
+<script src='/js/list.js'></script>{{> header}}
+
+<main class='container'>
+  {{> breadcrumb}}
+  <div class='card p-2 mt-4'>
+    {{> search}}
+  </div>
+  <div class='mt-5 row'>
+    <div class='col-md-3'>
+      <div class='card'>
+        <div class='card-header'>Filter</div>
+        <div class='card-body'>
+          <div class='mb-3'>
+            <label for='exampleFormControlInput1' class='form-label fw-bold'>Bus
+              Operator</label>
+            <select class='form-select' aria-label='Default select example' id="filter-bus-operator">
+              <option value="" selected>All</option>
+            </select>
+          </div>
+          <div class='mb-3'>
+            <label for='exampleFormControlInput1' class='form-label fw-bold'>Types of seat</label>
+            <div class='form-check'>
+              <input class='form-check-input' type='radio' value='0' name="typeOfSeat" />
+              <label class='form-check-label' for='flexCheckDefault'>
+                Limousine
+              </label>
+            </div>
+            <div class='form-check'>
+              <input class='form-check-input' type='radio' value='1' checked name="typeOfSeat" />
+              <labe
+<div class='mt-5 row'>
+    <div class='col-md-3'>
+      <div class='card'>
+        <div class='card-header'>Filter</div>
+        <div class='card-body'>
+          <div class='mb-3'>
+            <label for='exampleFormControlInput1' class='form-label fw-bold'>Bus
+              Operator</label>
+            <select class='form-select' aria-label='Default select example' id="filter-bus-operator">
+              <option value="" selected>All</option>
+            </select>
+          </div>
+          <div class='mb-3'>
+            <label for='exampleFormControlInput1' class='form-label fw-bold'>Types of seat</label>
+            <div class='form-check'>
+              <input class='form-check-input' type='radio' value='0' name="typeOf
